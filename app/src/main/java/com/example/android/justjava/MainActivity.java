bcd59d1c35c9df04cf51199b32845cf4e685f262
Benjamin Fras <bfr@easycow.de>
2017-07-06 21:39:47 +0200

    add whipped cream

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
+    private static final String TAG = "MainActivity";
     private GoogleApiClient client;
 
     @Override
@@ -34,40 +37,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method retrieves the TextView that displays the quantity
-     *
-     * @return
-     */
-    public TextView getQuantityTextView() {
-        return (TextView) findViewById(R.id.quantity_text_view);
-    }
-
-    /**
-     * This method retrieves the TextView that displays the message
-     *
-     * @return
-     */
-    public TextView getOrderSummaryTextView() {
-        return (TextView) findViewById(R.id.order_summary_text_view);
-    }
-
-    /**
-     * This method retrieves the Button that decreases the quantity
-     *
-     * @return
-     */
-    public View getButtonDecrease() {
-        return (View) findViewById(R.id.button_decrease);
-    }
-
-    /**
      * This method retrieves the numeric value of the TextView that displays the quantity
      *
      * @return
      */
     private int getQuantity() {
-        int quantity = Integer.valueOf(getQuantityTextView().getText().toString());
-        return quantity;
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        return Integer.valueOf(quantityTextView.getText().toString());
     }
 
     /**
@@ -76,10 +52,11 @@ public class MainActivity extends AppCompatActivity {
      * @param quantity
      * @return
      */
-    private String createOrderSummary(int price, int quantity) {
+    private String createOrderSummary(int price, int quantity, boolean addWhippedCream) {
         Resources res = getResources();
-        return String.format("%s: %s\n%s: %d\n%s: %d €\n%s!",
+        return String.format("%s: %s\n%s? %s\n%s: %d\n%s: %d €\n%s!",
                 res.getString(R.string.name), "Kapitain Kunal",
+                res.getString(R.string.whippedcream), (addWhippedCream ? res.getString(R.string.yes) : res.getString(R.string.no)),
                 res.getString(R.string.quantity), quantity,
                 res.getString(R.string.total), price * quantity,
                 res.getString(R.string.thanks));
@@ -91,20 +68,19 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         int priceForCoffee = 5;
 
-//        Resources res = getResources();
-//        String priceMessage = String.format("%s: %d €\n%s!", res.getString(R.string.total), getQuantity()*priceForCoffee, res.getString(R.string.thanks));
-//
-//        display(getQuantity());
-//        displayPrice(getQuantity()*priceForCoffee);
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedcream_checkbox_view);
+        boolean hasWhippedCream = whippedCream.isChecked();
 
-        displayMessage(createOrderSummary(priceForCoffee, getQuantity()));
+        Log.v(TAG, "Has whipped cream: " + hasWhippedCream);
+        displayMessage(createOrderSummary(priceForCoffee, getQuantity(), hasWhippedCream));
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        getQuantityTextView().setText("" + number);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 
     /**
@@ -113,17 +89,19 @@ public class MainActivity extends AppCompatActivity {
      * @param message
      */
     private void displayMessage(String message) {
-        getOrderSummaryTextView().setText("" + message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText("" + message);
     }
 
     /**
      * This method increases the value of quantity by 1
      */
     public void increment(View view) {
+        Button decreaseButton = (Button) findViewById(R.id.button_decrease);
         display(getQuantity() + 1);
 
         if (getQuantity() + 1 > 0) {
-            getButtonDecrease().setEnabled(true);
+            decreaseButton.setEnabled(true);
         }
     }
 
@@ -131,10 +109,11 @@ public class MainActivity extends AppCompatActivity {
      * This method decreases the value of quantity by 1
      */
     public void decrement(View view) {
+        Button decreaseButton = (Button) findViewById(R.id.button_decrease);
         display(getQuantity() - 1);
 
         if (getQuantity() == 0) {
-            getButtonDecrease().setEnabled(false);
+            decreaseButton.setEnabled(false);
         }
     }
 
